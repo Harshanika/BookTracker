4f17401dda56187006fa82d3924520a75c069e21
Harshanika Thennakoon <harshanika@efuturesworld.com>
2025-08-17 14:28:21 +0530

    Add Other Module to App Module

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,9 +1,31 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 import { AppController } from './app.controller';
 import { BooksModule } from "./books/books.module";
+import { TypeOrmModule } from "@nestjs/typeorm";
+import { AuthModule } from "./auth/auth.module";
+import { UsersModule } from "./users/users.module";
+import { LendingModule } from "./lending/lending.module";
+import { DashboardModule } from "./dashboard/dashboard.module";
 
 @Module({
-    imports: [BooksModule],
+    imports: [
+        ConfigModule.forRoot({ isGlobal: true }), // Loads .env
+        TypeOrmModule.forRootAsync({
+            imports: [ConfigModule],
+            inject: [ConfigService],
+            useFactory: (config: ConfigService) => ({
+                type: 'postgres',
+                host: 'db', // Use 'db' as the service name in Docker
+                port: 5432,
+                username: config.get('DB_USER'),
+                password: config.get('DB_PASSWORD'),
+                database: config.get('DB_NAME'),
+                autoLoadEntities: true,
+                synchronize: true,
+            }),
+        }),
+        BooksModule, AuthModule, UsersModule, LendingModule, DashboardModule],
     controllers: [AppController],
     providers: [],
 })
